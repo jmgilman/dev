74803a05c57ee0a032ca24a105e30c297296c141
Joshua Gilman <joshuagilman@gmail.com>
2022-02-24 22:48:58 -0800

    ci: adds CI  workflow
    * ci: adds CI workflow for Github Actions
    
    * ci: add workflow file as trigger
    
    * refactor: changes flake to be more pure by installing all poetry deps
    
    * fix: fixes incorrect cache name and malformed command string
    
    * ci: adds mdformat and build test

--- flake.nix
+++ flake.nix
@@ -2,19 +2,36 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix-src.url = github:nix-community/poetry2nix;
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix-src }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+
+        pkgs = import nixpkgs {
+          inherit system;
+          # Overrides poetry to use the specified python version
+          # see: https://github.com/nix-community/poetry2nix/blob/master/overlay.nix
+          overlays = [ poetry2nix-src.overlay ];
+        };
+
+        app = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+          python = pkgs.python310;
+          preferWheels = true;
+          # Common overrides to make python package builds more reliable
+          # see: https://github.com/nix-community/poetry2nix/blob/master/overrides/default.nix
+          overrides = [
+            pkgs.poetry2nix.defaultPoetryOverrides
+          ];
+        };
       in {
         devShell = pkgs.mkShell {
           buildInputs = [
             pkgs.mdl
-            pkgs.python310 (
-              pkgs.poetry.override { python = pkgs.python310; }
-            )
           ];
+          inputsFrom = [app];
         };
       });
 }
