295a2ef5f9972dbf04ae25ed268a30e0ff8420ef
Joshua Gilman <joshuagilman@gmail.com>
2022-03-04 22:38:56 -0800

    build(nix): changes poetry2nix to use env

--- flake.nix
+++ flake.nix
@@ -2,36 +2,32 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    poetry2nix-src.url = github:nix-community/poetry2nix;
+    poetry2nix-src.url = github:jmgilman/poetry2nix;
   };
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix-src }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-
         pkgs = import nixpkgs {
           inherit system;
-          # Overrides poetry to use the specified python version
-          # see: https://github.com/nix-community/poetry2nix/blob/master/overlay.nix
           overlays = [ poetry2nix-src.overlay ];
         };
 
-        app = pkgs.poetry2nix.mkPoetryApplication {
+        poetry = pkgs.poetry2nix.mkPoetryEnv {
           projectDir = ./.;
           python = pkgs.python310;
           preferWheels = true;
-          # Common overrides to make python package builds more reliable
-          # see: https://github.com/nix-community/poetry2nix/blob/master/overrides/default.nix
           overrides = [
             pkgs.poetry2nix.defaultPoetryOverrides
           ];
         };
       in {
         devShell = pkgs.mkShell {
-          buildInputs = [
+          packages = [
+            poetry
             pkgs.mdl
           ];
-          inputsFrom = [app];
         };
-      });
+      }
+    );
 }
